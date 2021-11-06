cb4ec45ff27264214ef200e2aecedb26cf094e9d
Arvind_mac <greenray771@gmail.com>
2021-11-06 18:05:10 -0400

    Added multipage system.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 import logo from './logo.svg';
+import Movie_List from './Components/Movie_List';
+import Movie_Statistics from './Components/Movie_Statistics';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        BrowserRouter >
+        <
+        div >
+        <
+        div >
+        <
+        Link className = "App-link"
+        to = "/" > Home < /Link> | <Link className="App-link" to="/page
+        1 ">Page 1</Link> | <
+        Link className = "App-link"
+        to = "/page3" > Page 3 < /Link> <
+        /div> <
+        Switch >
+        <
+        Route exact path = "/" >
+        <
+        Movie_List / >
+        <
+        /Route> <
+        Route path = "/page1" >
+        <
+        h2 > Welcome to page 1 < /h2> <
+        /Route> <
+        Route path = "/page3" >
+        <
+        h3 > Welcome to page 3 < /h3> <
+        Route path = "/page3/:name" >
+        <
+        Movie_Statistics / >
+        <
+        /Route> <
+        /Route> <
+        /Switch> <
+        /div> <
+        /BrowserRouter> <
+        /header> <
+        /div>
+    );
 }
 
 export default App;
---
+++ src/Pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="App-header">
+            <h4>
+                This is a HackRPI project created by Arvind Rathnashyam and Henry Ye.
+            </h4>
+        </div>
+    )
+}
