43689c8cdeb1ca797d4dd6d15a944a70576686f1
Henry <yehenry11@gmail.com>
2021-11-06 13:44:33 -0400

    Added webscraper for metacritic

--- api/api.py
+++ api/api.py
@@ -1,8 +1,11 @@
 from flask import Flask
 import time
+import web_scraper
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+@app.route('/movie_data')
+def scrape_web():
+    top_movies = 'https://www.metacritic.com/browse/movies/release-date/theaters/date'
+    data = web_scraper.get_url_data(top_movies, 10)
+    return { 'movie_data': str(data) }
---
+++ api/web_scraper.py
@@ -0,0 +1,51 @@
+import json
+import ssl
+
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+def get_content(url):
+    req = Request(url, headers={ 'User-Agent': 'Moazilla/5.0' })
+    page = urlopen(req).read()
+    
+    soup = BeautifulSoup(page, 'html.parser')
+    return soup
+
+def get_data(tr):
+    data = {}
+
+    a = tr.find('a', attrs={ 'class': 'title' })
+    if a != None:
+        data['title'] = a.find('h3').text
+    
+    div = tr.find('div', attrs={ 'class': 'clamp-details' })
+    if div != None:
+        data['date'] = div.find('span').text
+
+    return data
+
+def get_url_data(url, num_searches):
+    content = get_content(url)
+
+    tables = content.findAll('table', attrs={ 'class': 'clamp-list' })
+
+    all_data = []
+
+    i = 0
+    for table in tables:
+        for tr in table.findAll('tr'):
+            data = get_data(tr)
+            if len(data) > 0:
+                all_data.append(data)
+                i += 1
+            
+            if i >= num_searches:
+                tables.clear()
+                break
+
+    return all_data
