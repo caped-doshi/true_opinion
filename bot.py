f6d6501e2ae65d78f63bf4ae6f35be170216191c
caped-doshi <arvind27kr@gmail.com>
2021-12-15 16:23:50 -0500

    Fuzzy Seach top 5 matches to query.

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import pymongo
 from pymongo import MongoClient
 from dotenv import load_dotenv
 from os import getenv
+from FuzzySeach import SearchAlgorithm
 load_dotenv()
 
 bot = discord.Bot()
@@ -16,7 +17,6 @@ collection = db.get_collection("imdb")
 async def find_by_id(id):
     cursor = await collection.find_one({'_id':id})
     return cursor
-    #return item
 
 @bot.slash_command(guild_ids=[748940193733804252])
 async def hello(ctx, name):
@@ -30,10 +30,12 @@ async def add(ctx, num1: int, num2: int):
 async def ping(ctx):
     await ctx.respond('pong')
 
-@bot.slash_command(name='avengers',guild_ids=[748940193733804252])
-async def avengers(ctx):
-    score = await find_by_id('avengers')
-    await ctx.respond('Avengers Score: ' + str(score['score']))
+@bot.slash_command(name='search',guild_ids=[748940193733804252])
+async def search_result(ctx, query: str):
+    movies_arr = await find_by_id('all_movies')
+    algo = SearchAlgorithm(movies_arr['arr'])
+    top_5_arr = await algo.top_5(query)
+    await ctx.respond("These are your top 5 results from your search: " + ", ".join(top_5_arr))
 
 @bot.slash_command(name='all_movies', guild_ids=[748940193733804252])
 async def all_movies(ctx):
